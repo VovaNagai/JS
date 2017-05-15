27d3d3254fea3eed03f5d0565e3934c89fc887b3
falcon <ghostfalcon.kr@gmail.com>
2017-05-16 00:47:07 +0300

    hw1

---
+++ hw1.1/js/user.js
@@ -0,0 +1,9 @@
+var vova = {
+        name: 'Vova',
+        number: 3803333333333,
+        vova_info: {
+            vk: 'info@vk.ru',
+            mail: ['ghost@gmail.com', 'falcon@gmail.com']
+        },
+        confirmation: true
+};
