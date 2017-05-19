aeb07a97a2eb83f1148a466d82570d8a03c9b449
falcon <ghostfalcon.kr@gmail.com>
2017-05-19 16:44:17 +0300

    Commit submerge/refactoring done

---
+++ lesson3/homework3/js/user.js
@@ -0,0 +1,11 @@
+
+/*let numbers = [25, 1];
+let project = 'проект';
+let team = `команда`;
+let howMuch = 'Сколько';
+let sentence = 'нужно программистов чтобы сделать проект ?';
+// const helloWord = `hello ${iAmString}`;
+let task1 = `${howMuch} ${sentence} ${numbers[1]}, ${numbers[0]}, ${team}`;
+console.log(task1);*/
+
+
