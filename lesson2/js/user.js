fd244fb053533c76cf1c8759c2231116e992ef96
falcon <ghostfalcon.kr@gmail.com>
2017-05-19 17:21:55 +0300

    Clean up the submodule mess: step 2

---
+++ lesson2/js/user.js
@@ -0,0 +1,66 @@
+// const string = 'some string';
+// const iAmString = "second string";
+// const es6String = `hello world`;
+//
+// console.log(string, iAmString, es6String);
+//
+// const helloWord = `hello ${iAmString}`;
+// -------------------------------------------------------------------------------------------------
+// var myString = 'vova';
+// console.log(myString[0].toUpperCase(), myString[0]);
+// -----------------------------------------------------------------------------------------------
+// var user = {
+//     cars: {
+//         name: ['honda' + ' lada' + ' nisan' + "."],
+//         your: 'Vova'
+//     }
+// };
+// console.log('У пользователя ' + user.cars.your + ' есть машина ' + user.cars.name);
+// -----------------------------------------------------------------------------------------
+// var user = {
+//     cars: {
+//         name: ['honda' + ' lada' + ' nisan' + "."],
+//         your: 'Vova'
+//     }
+// };
+// console.log(`У пользователя ${user.cars.your}` + ` есть машина ${user.cars.name}`);
+//
+//
+
+// let myName = 'vOVA';-------------------------------------------------------------------------
+// console.log(myName[0].toUpperCase() + myName.toLocaleLowerCase().slice(1));
+// console.log(myName.toLocaleLowerCase() + myName.slice(3));
+
+// // SPLIT (разделитель) - превращает строку в массив-----------------------------------------------
+// let myName = 'Vova';
+// let arrray = myName.split('');
+// console.log(arrray);
+
+// REPLACE ()-------------------------------------------------------------------------
+// const hi = 'privet kak dela';
+// let array = hi.replace('dela', 'ti');
+// console.log(array);
+
+// INDEX OF -----------------------------------------------------------------------------
+// var hi = 'privet kak dela';
+// var array = hi.replace('dela', 'ti');
+// console.log(hi.indexOf(','));
+//----------------------------------------------------------------------------------------------------
+
+// == || ===
+
+// var number = '50';
+// console.log(number == 50);
+// var number2 = '50';
+// console.log(number2 === 50);
+//-----------------------------------------------------------------------------------------------------
+// var number =  'year2017';
+// console.log(number.slice(6)); //one method
+// console.log(2017%2000);  //two method
+//--------------------------------------------------------------------------------------------------------
+// var number =  'year2017';
+// console.log(!true); // window false
+//----------------------------------------------------------------------------------------------------
+var someArray = [100, 200, 300, 400];
+console.log(Math.max(someArray));
+
