1056b14051e6f934615decd9a4c23073c5ede107
falcon <ghostfalcon.kr@gmail.com>
2017-06-12 20:50:37 +0300

    I start to do homework9

---
+++ lesson9/homework9/js/user.js
@@ -0,0 +1,283 @@
+'use strict';
+//вывести имя друга и какие он любит фрукты
+// использовать так же уже существующий метод ShowFruits
+//
+const user = {
+    name: 'Jack',
+    fruits: 'mango, kivi, banan',
+    friends: [
+        {
+            name: 'Egor',
+            fruits: 'lime'
+        },
+        {
+            name: 'Roma',
+            fruits: 'Orange'
+        },
+        {
+            name: 'Lera',
+            fruits: 'Banan'
+        }
+    ],
+    showFruits: function () {
+        //console.log(this.fruits);
+        return this.fruits;
+    },
+    showFriendsFruits: function () {
+        let ffr = this.friends.map(elem => elem.fruits);
+        //console.log(ffr.join(', '));
+        return ffr.join(', ');
+    }
+};
+
+console.log(user.showFruits());
+console.log(user.showFriendsFruits());
+// // Legolas Task !
+//
+// TASK 1
+// Отобразите достаточно ли у developers навыков ?
+// Отобразите всех разработчиков и вызовете у каждого
+// разработчика метод goodDev --
+
+/*
+ Количество требований к разработчику совпадает с его навыками.
+ * Используйте в задаче this
+ * */
+
+let developer1 = {
+    skills: ['JavaScript', 'linux', 'html', 'OOP', 'Node.js'],
+    requires: ['Node.js', 'JavaScript', 'OOP'],
+    goodDev: goodDev
+};
+let developer2 = {
+    experience: [
+        { technology: 'java' },
+        { technology: 'c++' },
+        { technology: 'aws' },
+        { technology: 'docker' }
+    ],
+    requires: ['java', 'json', 'c++', 'JavaScript'],
+    goodDev: goodDev
+};
+
+function goodDev() {
+    this.requires.forEach(req => {
+        let expz = ('experience' in this) ? this.experience.map(exp => exp.technology) : (('skills' in this) ? this.skills : []);
+        let got = (expz.some(exp => exp.toLowerCase() == req.toLowerCase())) ? 'success' : 'fail';
+        console.log(`required: ${req} ... ${got}`);
+    });
+}
+
+let developers = [developer1, developer2];
+developers.forEach((dev, index) => {
+    console.log(`developer ${index + 1}`);
+    dev.goodDev();
+});
+// developer 1
+// required: Node.js ... success
+// required: JavaScript ... success
+// required: OOP ... success
+// ---
+
+// developer 2
+// required: json ... fail
+// required: JavaScript ... success
+// required: Java ... success
+// required: OOP ... success
+
+/*
+ *
+ * TASK 2
+ *
+ * Создайте объект содержащий коллекцию элементов с одинаковыми
+ * свойстами и разными значениями!
+ * Напишите функцию принимает 1 аргумент сортирует объект по
+ * переданному значению
+ *
+ * При вызове функции используйте this
+ *
+ * */
+
+let myObject = {
+    // database: [
+    //     { age: 100, name: 'b' },
+    //     { age: 15, name: 'c' },
+    //     { age: 25, name: 'a' }
+    // ],
+    // myFilter(){
+    //   let filterDataSorts = this.database.forEach
+    // }
+};
+
+// {age:15, name:'c'}, {age:25, name:'a'} {age:100, name:'b'}
+
+console.log(myObject.myFilter('age'));
+console.log(myObject.myFilter('name'));
+
+// {age:25, name:a}, {age:100, name: b} ...
+
+/*
+ * TASK 3
+ *
+ * Перепишите homework 5 с использованием методов массивов и
+ * => arrow functions
+ *
+ *
+ */
+/*
+ 1. Переместите 0 в конец массива, остальные числа должны остаться
+ неизменными
+ .сoncat();
+ example:
+ [1,false,2,0,3,null,0,4,0,25] => [1, false, 2, 3, null, 4, 25, 0, 0, 0]
+ [ 'a', 0, 0, 'b', null, 'c', 'd', 0, 1, false, 0, 1, 0, 3, [], 0, 1, 9, 0, 0, {}, 0, 0, 9 ] => ["a","b",null,"c","d",1,false,1,3,[],1,9,{},9,0,0,0,0,0,0,0,0,0,0]
+ [ 0, 1, null, 2, false, 1, 0 ] => [1,null,2,false,1,0,0]
+ */
+
+let arr1 = [1, false, 2, 0, 3, null, 0, 4, 0, 25];
+
+//Метод счетчика
+function moveZeroToEnd(arr) {
+    var newArray = [],
+        zeroes = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            zeroes++;
+        } else {
+            newArray.push(arr[i]);
+        }
+    }
+    for (var i = 0; i < zeroes; i++) {
+        newArray.push(0);
+    }
+    return newArray;
+}
+
+console.log('1.1: ', moveZeroToEnd(arr1));
+
+//Метод concat + map
+
+let arr2 = [ 0, 1, null, 2, false, 1, 0 ];
+
+function moveZeroToEnd_2(arr) {
+    var newArray = [],
+        zeroes = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            zeroes++;
+        } else {
+            newArray.push(arr[i]);
+        }
+    }
+    newArray = newArray.concat(Array.from({length:zeroes}).map(function(v){return 0;}));
+    return newArray;
+}
+
+console.log('1.2: ', moveZeroToEnd_2(arr2));
+
+/*
+ 2. Верните сумму двух найменьших чисел в массиве
+
+ [10,20,30,1,31,11,10] => 11
+ [-1,0,25] => -1
+ [-4,-10,25,10] => -14
+ [0,200,10,25,15] => 10
+
+ */
+
+let arr = [10,20,30,1,31,11,10];
+// let functionArr = (minNumber) => {
+//
+//     return minNumber.filter(function (elem, index) {
+//
+//         }
+//     });
+//
+// };
+
+/*
+ 3. Напишите функцию которая меняет местами имя и фамилию
+ nameShuffler('john McClane'); => "McClane john"
+ nameShuffler('Arnold Schwarzenegger'); => "Schwarzenegger Arnold"
+ nameShuffler('James Bond'); => "Bond James"
+ */
+
+let nameShuffler = (arg) => {
+    let pieces = arg.split('');
+    pieces.reverse();
+    return pieces.join('');
+};
+console.log('3.1: ',nameShuffler('john McClane'));
+console.log('3.2: ',nameShuffler('James Bond'));
+
+/*
+ 4. Напишите функцию которая принимает массив с именами и возвращает массив
+ в котором каждая буква становится заглавной
+ capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+ capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+ */
+
+let capMe = (arg) => {
+    var newArr = [];
+    for (var i = 0; i < arg.length; i++) {
+        var c = arg[i].slice(0,1).toUpperCase() + arg[i].slice(1).toLowerCase();
+        newArr.push(c);
+    }
+    return newArr;
+};
+
+console.log('4: ',capMe(['jo', 'nelson', 'jurie']));
+
+
+//
+//
+//
+
+
+////////@ TODO -- LVL Strong Junior
+/*
+ *
+ * TASK 1
+ * Напишите функцию которая принимает 3 аргумента:*
+ *
+ *  - объект к которому привязывается метод
+ *  - Имя свойства с которым связывается метод
+ *  - Объявление привязываемого метода( функция )
+ *
+ *
+ *  Если количество аргументов у функции fn совпадает с переданными
+ *  параметрами тогда сохраняет метод в замыкании
+ *  и привязывает функцию к методу объекта
+ *
+ *  при вызове одного и того же метода с разным количеством аргументов,
+ *  должно давать различный результат
+ *
+ * */
+
+let junior = {};
+
+// fn.length == arguments.length
+
+function addMethod(object, name, fn) {
+    object[name] = fn;
+}
+function newMethodFun() {
+    
+}
+addMethod(junior, 'ok', function() {
+    console.log('zero arguments');
+});
+addMethod(junior, 'ok', function(one) {
+    console.log('one arguments');
+});
+addMethod(junior, 'ok', function(one, two) {
+    console.log('two arguments');
+});
+addMethod(junior, 'ok', function(one, two, three) {
+    console.log('three arguments');
+});
+
+junior.ok(); //'zero arguments'
+junior.ok(1); //'one arguments'
+junior.ok(1, 2); // 'two arguments'
+junior.ok(1, 2, 3); // 'three arguments'
